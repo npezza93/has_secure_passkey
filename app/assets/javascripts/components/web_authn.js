0f1bfe9ac9f6c579a34521dbf499270a59e5441c
Nick Pezza <pezza@hey.com>
2024-11-04 11:13:11 -0500

    Remove id

--- app/assets/javascripts/components/web_authn.js
+++ app/assets/javascripts/components/web_authn.js
@@ -11,7 +11,6 @@ export default class WebAuthn extends HTMLElement {
   connectedCallback() {
     this.progressBar = Turbo.navigator.delegate.adapter.progressBar
     this.style.display = "none"
-    this.setAttribute("id", "webauthn")
     this.setAttribute("data-turbo-temporary", 1)
     this.run()
   }
