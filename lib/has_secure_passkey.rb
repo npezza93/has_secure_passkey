91a33121817f56d7ebc26e9fbe4e1e5354be2fba
Nick Pezza <pezza@hey.com>
2025-11-22 19:19:01 -0500

    Add missing require

--- lib/has_secure_passkey.rb
+++ lib/has_secure_passkey.rb
@@ -5,6 +5,7 @@ require "has_secure_passkey/engine"
 require "has_secure_passkey/options_for_create"
 require "has_secure_passkey/options_for_get"
 require "has_secure_passkey/active_record_helpers"
+require "has_secure_passkey/recovery"
 require "has_secure_passkey/authenticate_by"
 require "has_secure_passkey/add_passkey"
 
